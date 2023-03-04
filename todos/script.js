0db114f7740cafe8a2d9818b78bf882fe6760c2d
Filip <filip.ratkovic.fr@gmail.com>
2023-03-04 19:40:06 +0100

    Sent data to locale storage

--- todos/script.js
+++ todos/script.js
@@ -2,6 +2,10 @@ const formTodo = document.getElementById('form');
 const input = document.getElementById('input');
 const todosUL = document.getElementById('todos');
 
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+
+
 formTodo.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -11,29 +15,54 @@ formTodo.addEventListener('submit', (e) => {
 const addTodo = (todo) => {
     let todoText = input.value;
 
-    if(todo) {
+    if (todo) {
         todoText = todo.text;
     }
 
-    if(todoText) {
+    if (todoText) {
         const todoEL = document.createElement('li');
         if (todo && todo.completed) {
-            todoEL.classList.add('completed')
+            todoEL.classList.add('completed');
         }
+
+        todoEL.innerText = todoText;
 
         todoEL.addEventListener('click', () => {
             todoEL.classList.toggle('completed')
+            updateLS()
         })
 
-        todoEL.addEventListener( 'contextmenu', (e) => {
+        todoEL.addEventListener('contextmenu', (e) => {
             e.preventDefault();
-            todoEL.remove()
+            todoEL.remove();
+            updateLS()
+
         })
 
-        
 
-        todoEL.innerText = todoText;
+
         todosUL.appendChild(todoEL);
-        input.value='';
+        input.value = '';
+
+        updateLS()
     }
+}
+
+if (todos) {
+    todos.forEach((todo) => {
+       addTodo(todo)
+    })
+}
+function updateLS() {
+    let todosEl = document.querySelectorAll('li');
+    const todos = [];
+
+    todosEl.forEach((todoEl) => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
