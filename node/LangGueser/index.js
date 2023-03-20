dfe50726af9a6a0f124c90e9e46d8ffe4d6c772c
Filip <filip.ratkovic.fr@gmail.com>
2023-03-20 22:40:31 +0100

    Language guesser nodeJs vezba

--- node/LangGueser/index.js
+++ node/LangGueser/index.js
@@ -1,1 +1,8 @@
-const franc = required('franc');
+const langs = module.require('langs');
+const franc = module.require('franc');
+const langTry = franc('Please tell me this')
+
+ const language = langs.where("name", "Korean");
+
+console.log(language.name)
+
